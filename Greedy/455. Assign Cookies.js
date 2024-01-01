98b4543459a69e37a0d8dba7b353cb36416b7bbc
kendoow <kendo_ow@mail.ru>
2024-01-01 17:25:52 +0300

    1.1.2024

--- Greedy/455. Assign Cookies.js
+++ Greedy/455. Assign Cookies.js
@@ -1,8 +1,13 @@
-const findContentChildren = (g, s) => {
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function (g, s) {
   g.sort((a, b) => a - b);
   s.sort((a, b) => a - b);
-  let j = 0,
-    res = 0;
+  let j = 0
+  let res = 0;
   for (let num of s) {
     if (num >= g[j]) res++, j++;
   }
