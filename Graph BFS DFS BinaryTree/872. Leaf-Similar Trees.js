d2a3e9192471891a6323222a3c08fc48cb6ba3db
kendoow <kendo_ow@mail.ru>
2024-01-09 12:42:52 +0300

    9.1.2024

--- Graph BFS DFS BinaryTree/872. Leaf-Similar Trees.js
+++ Graph BFS DFS BinaryTree/872. Leaf-Similar Trees.js
@@ -1,20 +1,20 @@
 const leafSimilar = (root1, root2) => {
-    let leaf1 = [];
-    let leaf2 = [];
-
-    let dfs = function(node, leaf) {
-        if (!node) return;
-        if (!node.left && !node.right) {
-            leaf.push(node.val);
-            return;
+    const arr1 = []
+    const arr2 = []
+    const dfs = (node, arr) => {
+        if (!node) {
+            return
         }
-        dfs(node.left, leaf);
-        dfs(node.right, leaf);
+        if (!node.left && !node.right) {
+            arr.push(node.val)
+            return
+        }
+        dfs(node.left, arr)
+        dfs(node.right, arr)
     }
-
-    dfs(root1, leaf1)
-    dfs(root2, leaf2);
-    return leaf1.join('_') == leaf2.join('_')
+    dfs(root1, arr1)
+    dfs(root2, arr2)
+    return arr1.join('') === arr2.join('')
 };
 
-console.log(leafSimilar([3,5,1,6,2,9,8,null,null,7,4],[3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]))
+console.log(leafSimilar([3, 5, 1, 6, 2, 9, 8, null, null, 7, 4], [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8]))
